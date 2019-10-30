5a2929d377cb9d536e7cbf242217e5f41382037e
Brennan Pilcher <brennan.pilcher@thoughtworks.com>
2019-10-30 15:18:02 -0400

    Add method to parse coordinates from input and refactor the input test file.

--- __tests__/IO.spec.js
+++ __tests__/IO.spec.js
@@ -3,9 +3,15 @@ import IO from '../src/IO';
 describe('IO module', () => {
 
   it('should import the contents of a file', async () => {
-    const testData = await IO.getRawInput("__tests__/test_data/inputFile.txt");
+    const input = await IO.getRawInput("__tests__/test_data/inputFile.txt");
 
-    expect(testData).toEqual('this is some test data');
+    expect(input).toEqual('1 2 N\nMLMRML');
   });
+
+  it('should parse initial coordinates from the input', () => {
+    const parsedInput = IO.parseCoordinatesFromInput('1 2 N\nMLMRML');
+
+    expect(parsedInput).toEqual({ x: 1, y: 2 });
+  })
 
 });
--- src/IO.js
+++ src/IO.js
@@ -5,6 +5,12 @@ const IO = {
     return fs.promises.readFile(filename, 'utf8', (err, data) => {
       return data;
     });
+  },
+  parseCoordinatesFromInput: (input) => {
+    if ((/^\d+\s\d+\s[NESW]\n[LRM]+$/).test(input)) {
+      const coordinates = input.match(/\d+/g);
+      return { x: parseInt(coordinates[0]), y: parseInt(coordinates[1]) };
+    }
   }
 }
 
