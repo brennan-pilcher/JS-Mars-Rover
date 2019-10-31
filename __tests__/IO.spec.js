003da7d845fa7816310a56a1c1165d1a0e01c4c5
Brennan Pilcher <brennan.pilcher@thoughtworks.com>
2019-10-31 12:02:20 -0400

    Create additional test data and refactor the input file. Refactor existing tests to use new/updated test data.

--- __tests__/IO.spec.js
+++ __tests__/IO.spec.js
@@ -1,24 +1,25 @@
 import IO from '../src/IO';
+import { firstRoverInput, invalidInputs, rawInput } from '../test_data/inputs';
 
 describe('IO module', () => {
 
   it('should import the contents of a file', async () => {
-    const input = await IO.getRawInput("__tests__/test_data/inputFile.txt");
+    const input = await IO.getRawInput("test_data/inputFile.txt");
 
-    expect(input).toEqual('1 2 N\nMLMRML');
+    expect(input).toEqual(rawInput);
   });
 
   it('should parse initial coordinates and direction from the input', () => {
-    const parsedInput = IO.parseInitialStateFromInput('1 2 N\nMLMRML');
+    const parsedInput = IO.parseInitialStateFromInput(firstRoverInput);
 
-    expect(parsedInput).toEqual({ commands: 'MLMRML', direction: 'N', x: 1, y: 2 });
+    expect(parsedInput).toEqual({ commands: 'MLRMRMR', direction: 'N', x: 1, y: 3 });
   });
 
   it('should throw an error when parsing if the input is invalid', () => {
-    const invalidInput1 = (input) => {IO.parseInitialStateFromInput('1 2 X\nMLMRML')}
-    const invalidInput2 = (input) => {IO.parseInitialStateFromInput('1 2 2\nMLMRML')}
-    const invalidInput3 = (input) => {IO.parseInitialStateFromInput('1 2\nMLMRML')}
-    const invalidInput4 = (input) => {IO.parseInitialStateFromInput('1 2 N\nWWWW')}
+    const invalidInput1 = (input) => {IO.parseInitialStateFromInput(invalidInputs[0])}
+    const invalidInput2 = (input) => {IO.parseInitialStateFromInput(invalidInputs[1])}
+    const invalidInput3 = (input) => {IO.parseInitialStateFromInput(invalidInputs[2])}
+    const invalidInput4 = (input) => {IO.parseInitialStateFromInput(invalidInputs[3])}
 
     expect(invalidInput1).toThrow(Error('Unable to parse input: "1 2 X\nMLMRML"'));
     expect(invalidInput2).toThrow(Error('Unable to parse input: "1 2 2\nMLMRML"'));
---
+++ test_data/inputs.js
@@ -0,0 +1,30 @@
+export const rawInput =
+`6 6
+1 3 N
+MLRMRMR
+3 4 E
+MRMRMR
+2 3 W
+MRLLRLM`;
+
+export const grid = `6 6`;
+
+export const firstRoverInput = 
+`1 3 N
+MLRMRMR`;
+
+export const secondRoverInput = 
+`2 3 W
+MRLLRLM`;
+
+export const invalidInputs =
+[
+`1 2 X
+MLMRML`,
+`1 2 2
+MLMRML`,
+`1 2
+MLMRML`,
+`1 2 N
+WWWW`
+];
